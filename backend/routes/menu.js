65bba6f4226765225541a13336960d8b1b23fcb0
Palak0027 <shrivastavpalak495@gmail.com>
2025-08-22 00:40:25 +0530

    fixed login issue

--- backend/routes/menu.js
+++ backend/routes/menu.js
@@ -1,5 +1,7 @@
 // routes/menuRoutes.js
 import express from "express";
+import { requireAuth } from "../middleware/auth.js"; 
+
 import { addMenuItem, getMenuByRestaurantId } from "../controllers/MenuController.js";
 import multer from "multer";
 
