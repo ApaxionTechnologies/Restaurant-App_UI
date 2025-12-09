5a8a36d9230fc39246f05f04fa99c1920ec1c7ea
Palak0027 <shrivastavpalak495@gmail.com>
2025-12-09 23:42:06 +0530

    update register UI & translation part

---
+++ src/config/i18n.js
@@ -0,0 +1,26 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import Backend from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: "en",
+    supportedLngs: ["en", "hi"],
+    ns: ["common", "register"], // multiple namespaces
+    defaultNS: "common",
+    backend: {
+      loadPath: "/locales/{{lng}}/{{ns}}.json",
+    },
+    detection: {
+      order: ["localStorage", "navigator"],
+      caches: ["localStorage"],
+    },
+    interpolation: { escapeValue: false },
+  });
+  window.i18next = i18n;
+
+export default i18n;
